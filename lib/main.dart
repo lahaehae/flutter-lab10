8469ccb63dbb246d0d5dbc552cf498760d64accf
Aidyn Ibragimov <aidyn.ibragimov5322@gmail.com>
2025-04-26 20:21:41 +0500

    Make icon and apk-build

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    askNotificationPermission(); // <-- сюда вставляем вызов
+    askNotificationPermission();
   }
 
   void askNotificationPermission() async {
