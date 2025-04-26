181561923112124359995c1687e60383f2d45870
Aidyn Ibragimov <aidyn.ibragimov5322@gmail.com>
2025-04-26 16:57:29 +0500

    Working state

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'login_page.dart';
+import 'register_page.dart';
+import 'profile_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Auth App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/register': (context) => RegisterPage(),
+        '/profile': (context) => ProfilePage(),
+      },
+    );
+  }
+}
